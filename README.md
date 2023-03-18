8d579441ed3cba077a7abce65928c1aa68f86624
Ramamurthi <ramamurthi@192.168.1.7>
2023-03-18 23:35:34 +0530

    restarting datasets

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # geoguessr
 A n application to help with the game geoguessr
 
+# Downloading dataset
+Download kaggle creds from kaggle and store it in ~/.kaggle/kaggle.json
+
+```bash
+kaggle datasets download -d ubitquitin/geolocation-geoguessr-images-50k
+```
+
 #env setup
 printf "\n# Adding this command to read local .env file" >> env/bin/activate
 printf "\nexport \$(grep -v '^#' .env | xargs)" >> env/bin/activate
--- geoguessr/dataset.py
+++ geoguessr/dataset.py
@@ -1,33 +1,4 @@
 from torch.utils.data import Dataset
+from fastai import * 
 
-class GeoGuessrDataset(Dataset):
-    def __init__(self, ds, label_name, transform=None, target_transform=None):
-        
-        self.ds = ds
-        self.label_name = label_name
-        self.transform = transform
-        self.target_transform = target_transform
-        self.targets = ds[label_name]
-        self.classes_to_idx = {j:i for i,j in enumerate(set(ds[label_name]))}
-        self.idx_to_classes = {j:i for i,j in self.classes_to_idx.items()}
-        
-    def __len__(self):
-        return len(self.ds)
-
-    def __getitem__(self, idx):
-        
-        image = self.ds[idx]['image']
-        label = self.ds[idx][self.label_name]
-        
-        if self.transform:
-            image = self.transform(image)        
-        
-            
-        if self.target_transform:
-            label = self.target_transform(label)
-        
-        # Changing label to int using label mapper
-        label = self.classes_to_idx[label]
-        return image, label
-        
 
